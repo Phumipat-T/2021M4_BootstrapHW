8107305554a1b47901dfcc92444834163517f115
Phumipat Thirawat <phumipat_thirawat@elearning.cmu.ac.th>
2021-06-06 14:09:08 +0700

    Final commit
    Add js and finished

---
+++ script.js
@@ -0,0 +1,47 @@
+document.getElementById('item1').addEventListener('click',function(e){
+    alert('Plumeria')
+})
+
+document.getElementById('item2').addEventListener('click',function(e){
+    alert('Dahlia')
+})
+
+document.getElementById('item3').addEventListener('click',function(e){
+    alert('Lingopolo')
+})
+
+document.getElementById('item4').addEventListener('click',function(e){
+    alert('Rose')
+})
+
+document.getElementById('item5').addEventListener('click',function(e){
+    alert('Sunflower')
+})
+
+document.getElementById('item6').addEventListener('click',function(e){
+    alert('Blooming')
+})
+
+document.getElementById('item7').addEventListener('click',function(e){
+    alert('Daisy')
+})
+
+document.getElementById('item8').addEventListener('click',function(e){
+    alert('Leafed')
+})
+
+document.getElementById('item9').addEventListener('click',function(e){
+    alert('Pentaled')
+})
+
+document.getElementById('item10').addEventListener('click',function(e){
+    alert('Purity')
+})
+
+document.getElementById('item11').addEventListener('click',function(e){
+    alert('Lily')
+})
+
+document.getElementById('item12').addEventListener('click',function(e){
+    alert('Hibiscus')
+})
